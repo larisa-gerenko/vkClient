9ef523761d6e324ab0c76f92d006fe5fcb369365
Larisa <larisavalgutova@gmail.com>
2022-04-04 22:55:27 +0300

    add like

--- interface_ios/Cell/GalleryCollectionViewCell.swift
+++ interface_ios/Cell/GalleryCollectionViewCell.swift
@@ -10,9 +10,12 @@ import UIKit
 class GalleryCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var fotoImageView: UIImageView!
+    @IBOutlet weak var heartImageView: UIImageView!
+    @IBOutlet weak var counterLabel: UILabel!
     
     func configure(image: UIImage?) {
         fotoImageView.image = image
+        setup()
     }
     
     override func prepareForReuse() {
@@ -25,5 +28,34 @@ class GalleryCollectionViewCell: UICollectionViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    var count = 0
+    var isHeartPressed = false
+    
+    func setup() {
 
+        counterLabel.text = String(self.count)
+        heartState(isFilled: false)
+    }
+    
+    func heartState(isFilled: Bool) {
+        var heartImage = UIImage(systemName: "heart")
+        if isFilled {
+            heartImage = UIImage(systemName: "heart.fill")
+        }
+        self.heartImageView.image = heartImage
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+    
+        isHeartPressed = !isHeartPressed
+        heartState(isFilled: isHeartPressed)
+        
+        if isHeartPressed {
+            self.count += 1
+        } else {
+            self.count -= 1
+        }
+        counterLabel.text = String(self.count)
+    }
 }
